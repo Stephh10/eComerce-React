167a2eae7d79995aede9475eedb16b0ea53e25a2
Stefan <stephjes17@gmail.com>
2024-09-06 18:09:41 +0200

    Added admin  page

---
+++ admin/.vite/deps/react-bootstrap_Col.js
@@ -0,0 +1,13 @@
+"use client";
+import {
+  Col_default,
+  useCol
+} from "./chunk-RP67HCHB.js";
+import "./chunk-76OGPB3H.js";
+import "./chunk-4SFRHSJ3.js";
+import "./chunk-EQCVQC35.js";
+export {
+  Col_default as default,
+  useCol
+};
+//# sourceMappingURL=react-bootstrap_Col.js.map
